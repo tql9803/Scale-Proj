8c1741a82305157daf016ad14d6c287bca1739e9
Le Gia Bao <Le Gia Bao@LEGIABAO-PC>
2019-10-27 10:16:40 +0700

    Add project files.

---
+++ testport/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnGetSerialPorts = New System.Windows.Forms.Button()
+        Me.rtbIncomingData = New System.Windows.Forms.RichTextBox()
+        Me.Serial_Port1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ConnectBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnGetSerialPorts
+        '
+        Me.btnGetSerialPorts.Location = New System.Drawing.Point(30, 21)
+        Me.btnGetSerialPorts.Name = "btnGetSerialPorts"
+        Me.btnGetSerialPorts.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.btnGetSerialPorts.Size = New System.Drawing.Size(89, 22)
+        Me.btnGetSerialPorts.TabIndex = 0
+        Me.btnGetSerialPorts.Text = "port"
+        Me.btnGetSerialPorts.UseVisualStyleBackColor = True
+        '
+        'rtbIncomingData
+        '
+        Me.rtbIncomingData.Location = New System.Drawing.Point(144, 90)
+        Me.rtbIncomingData.Name = "rtbIncomingData"
+        Me.rtbIncomingData.Size = New System.Drawing.Size(192, 125)
+        Me.rtbIncomingData.TabIndex = 1
+        Me.rtbIncomingData.Text = ""
+        '
+        'Serial_Port1
+        '
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(187, 20)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(39, 13)
+        Me.Label11.TabIndex = 2
+        Me.Label11.Text = "Label1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(146, 49)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(189, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(36, 191)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(82, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(29, 60)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(60, 21)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'ConnectBtn
+        '
+        Me.ConnectBtn.Location = New System.Drawing.Point(39, 107)
+        Me.ConnectBtn.Name = "ConnectBtn"
+        Me.ConnectBtn.Size = New System.Drawing.Size(79, 20)
+        Me.ConnectBtn.TabIndex = 6
+        Me.ConnectBtn.Text = "Connect"
+        Me.ConnectBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(348, 262)
+        Me.Controls.Add(Me.ConnectBtn)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.rtbIncomingData)
+        Me.Controls.Add(Me.btnGetSerialPorts)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGetSerialPorts As Button
+    Friend WithEvents rtbIncomingData As RichTextBox
+    Friend WithEvents Serial_Port1 As IO.Ports.SerialPort
+    Friend WithEvents Label11 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ConnectBtn As Button
+End Class
---
+++ testport/Form1.vb
@@ -0,0 +1,58 @@
+﻿Imports System.Data.SqlClient
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.IO.Ports ' dung de cho dinh nghia port
+
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim k As Integer
+        Dim user As String
+        Dim total As Integer
+
+        Label11.Text = "Hi" + " " + Form2.TextBox1.Text + " , " + "Welcome you to PSA"
+        user = Form2.TextBox1.Text
+        total = 0
+        k = 0
+
+
+
+    End Sub
+    Private Sub DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles Serial_Port1.DataReceived
+        Dim inbuff As String
+        inbuff = Serial_Port1.ReadExisting() ' Receiving Value From the Serial Port
+        TextBox1.Text = inbuff
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Serial_Port1.WriteLine("Hello World!")
+    End Sub
+
+    Private Sub Port_press(sender As Object, e As EventArgs) Handles btnGetSerialPorts.Click
+
+        ComboBox1.Items.AddRange(IO.Ports.SerialPort.GetPortNames())
+        If ComboBox1.Items.Count <> 0 Then
+
+            Try
+                Serial_Port1.PortName = ComboBox1.Text
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End If
+
+
+    End Sub
+
+    Private Sub Connect_Pressed(sender As Object, e As EventArgs) Handles ConnectBtn.Click
+        If Serial_Port1.IsOpen Then
+            Serial_Port1.Close()
+        End If
+        Try
+
+            Serial_Port1.Open()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+End Class
+
+
